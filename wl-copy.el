76eaabc2cb417a3bea962ed777f92020cd3126d2
Hector Orellana <h.orellana@samtech.cl>
2025-01-12 11:35:15 -0300

    process-send-region

--- wl-copy.el
+++ wl-copy.el
@@ -7,14 +7,10 @@
    :command '("wl-copy")
    :connection-type 'pipe))
 
-(defun wl-copy/send-text (payload)
-  (let ((wl-copy-process (wl-copy/make-process)))
-    (process-send-string wl-copy-process payload)
-    (process-send-eof wl-copy-process)))
-
 (defun wl-copy/send-region ()
   (interactive)
-  (let ((selection (buffer-substring (region-beginning) (region-end))))
-    (wl-copy/send-text selection)))
+  (let ((wl-copy-process (wl-copy/make-process)))
+    (process-send-region wl-copy-process (region-beginning) (region-end))
+    (process-send-eof wl-copy-process)))
 
 (provide 'wl-copy)
